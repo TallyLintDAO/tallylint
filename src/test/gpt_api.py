7b5c29b874f1cf5e850296a3ba373e13b07c69f9
btwl <1253102853@qq.com>
2023-08-24 14:57:58 +0800

    update gpt api for rust backend

---
+++ src/test/gpt_api.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+import os
+from dotenv import load_dotenv
+import openai
+
+
+load_dotenv()
+OPENAI_KEY="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"
+# Set openai.api_key to the OPENAI environment variable
+openai.api_key = OPENAI_KEY
+
+
+completion = openai.ChatCompletion()
+
+def askgpt(question, chat_log=None):
+    if chat_log is None:
+        chat_log = [{
+            'role': 'system',
+            'content': 'You are a helpful, upbeat and funny assistant.',
+        }]
+    chat_log.append({'role': 'user', 'content': question})
+    response = completion.create(model='gpt-3.5-turbo', messages=chat_log)
+    answer = response.choices[0]['message']['content']
+    chat_log.append({'role': 'assistant', 'content': answer})
+    return answer, chat_log
+
+answer, log = askgpt('Why did the chicken cross the road?')
+print(answer)
+print(log)
