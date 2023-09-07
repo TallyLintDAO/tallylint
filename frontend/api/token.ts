60845da7b4cdc3976ddc68ecd657f03ab3be468e
e274426380 <867690443@qq.com>
2023-09-07 19:29:06 +0800

    frontend fixed count bug

--- frontend/api/token.ts
+++ frontend/api/token.ts
@@ -3,6 +3,7 @@ import { COINGECKO_URL } from "@/api/constants/ic";
 import { getCache, TTL } from "@/utils/cache";
 
 export const getICPPrice = async (timestamp: number): Promise<string | undefined> => {
+    //将小数点的时间戳转为整数时间戳
     timestamp = Math.floor(timestamp)
     //获取ICP的所有价格历史数据，并通过getCache保存到本地缓存中，ttl为1天，方便调用。
     const priceHistory = await getCache({
