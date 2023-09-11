5efc3e891c5a3105980ed113a21cb1e5ef33ef91
btwl <1253102853@qq.com>
2023-09-11 12:12:32 +0800

    upload dfx

---
+++ backend/scripts/git_add.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git add -f ./.dfx/local/canisters/backend/backend.did
+                                                         git add -f ./.dfx/local/canisters/backend/backend.did.d.ts  
+                                                         git add -f ./.dfx/local/canisters/backend/backend.did.js
+                                                         git add -f ./.dfx/local/canisters/backend/index.js
