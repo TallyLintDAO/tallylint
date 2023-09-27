bec0c53506d8c08eafd5c9abe0041bb3c9e1a388
e274426380 <867690443@qq.com>
2023-09-27 18:18:25 +0800

    update Dashboard

--- frontend/views/app/modules/Dashboard.vue
+++ frontend/views/app/modules/Dashboard.vue
@@ -10,88 +10,94 @@
 import { ref, onMounted } from "vue"
 import * as echarts from "echarts"
 import { getWalletHistory } from "@/api/rosetta"
+import { getUserWallet } from "@/api/user";
+import { showMessageError } from "@/utils/message";
 onMounted(() => {
   getWallet()
 })
 const getWallet = async () => {
-  const walletHistory = await getWalletHistory(
-    "9376e418870c3638d82f824211ec9e19e915f07f49e075834f56f3fcd3a8a05d",
-  )
-  console.log("history", walletHistory)
-  const timestamps = walletHistory.history.map((record) => new Date(Number(record.timestamp)).toLocaleString());
-  const balances = walletHistory.history.map((record) => record.walletValue);
-  // 基于准备好的dom，初始化echarts实例
-  var walletValueChart = echarts.init(document.getElementById("main"))
-  // 绘制图表
-  walletValueChart.setOption({
-    tooltip: {
-      trigger: "axis",
-      position: function (pt) {
-        return [pt[0], "10%"]
-      },
-    },
-    title: {
-      left: "center",
-      text: "Total Value",
-    },
-    toolbox: {
-      feature: {
-        dataZoom: {
-          yAxisIndex: "none",
+  const res = await getUserWallet(false);
+  if (res.Ok && res.Ok[0]) {
+    const walletAdress = res.Ok[0].address;
+    const walletHistory = await getWalletHistory(walletAdress)
+    console.log("history", walletHistory)
+    const timestamps = walletHistory.history.map((record) => new Date(Number(record.timestamp)).toLocaleString());
+    const balances = walletHistory.history.map((record) => record.walletValue);
+    // 基于准备好的dom，初始化echarts实例
+    var walletValueChart = echarts.init(document.getElementById("main"))
+    // 绘制图表
+    walletValueChart.setOption({
+      tooltip: {
+        trigger: "axis",
+        position: function (pt) {
+          return [pt[0], "10%"]
         },
-        restore: {},
-        saveAsImage: {},
       },
-    },
-    xAxis: {
-      type: "category",
-      boundaryGap: false,
-      data: timestamps,
-    },
-    yAxis: {
-      type: "value",
-      boundaryGap: false, //是否留白
-    },
-    dataZoom: [
-      {
-        type: "inside",
-        start: 0,
-        end: 100,
+      title: {
+        left: "center",
+        text: "Total Value",
       },
-      { //启用图表下方的进度条，方便拖放
-        start: 0,
-        end: 100,
-      },
-    ],
-    series: [
-      {
-        name: "Value",
-        type: "line",
-        symbol: "none",
-        smooth: true,
-        sampling: "lttb",
-        itemStyle: {
-          color: "rgb(255, 70, 131)",
+      toolbox: {
+        feature: {
+          dataZoom: {
+            yAxisIndex: "none",
+          },
+          restore: {},
+          saveAsImage: {},
         },
-        areaStyle: {
-          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            {
-              offset: 0,
-              color: "rgb(255, 158, 68)",
-            },
-            {
-              offset: 1,
-              color: "rgb(255, 70, 131)",
-            },
-          ]),
-        },
-        data: balances,
       },
-    ],
-  })
-  window.addEventListener('resize', () => {
-    walletValueChart.resize();
-  });
+      xAxis: {
+        type: "category",
+        boundaryGap: false,
+        data: timestamps,
+      },
+      yAxis: {
+        type: "value",
+        boundaryGap: false, //是否留白
+      },
+      dataZoom: [
+        {
+          type: "inside",
+          start: 0,
+          end: 100,
+        },
+        { //启用图表下方的进度条，方便拖放
+          start: 0,
+          end: 100,
+        },
+      ],
+      series: [
+        {
+          name: "Value",
+          type: "line",
+          symbol: "none",
+          smooth: true,
+          sampling: "lttb",
+          itemStyle: {
+            color: "rgb(255, 70, 131)",
+          },
+          areaStyle: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              {
+                offset: 0,
+                color: "rgb(255, 158, 68)",
+              },
+              {
+                offset: 1,
+                color: "rgb(255, 70, 131)",
+              },
+            ]),
+          },
+          data: balances,
+        },
+      ],
+    })
+    window.addEventListener('resize', () => {
+      walletValueChart.resize();
+    });
+  } else {
+    showMessageError('please add wallet for dashboard')
+  }
 }
 </script>
 
