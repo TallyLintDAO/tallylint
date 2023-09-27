047516d5cdb16463bd0fee8d6d0a14d7430db60c
e274426380 <867690443@qq.com>
2023-09-27 16:32:44 +0800

    add Dashboard value chart, and remove default Wallet.

---
+++ frontend/views/app/modules/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="dashboard-container">
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { ref, onMounted } from 'vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
