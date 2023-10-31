b93e33a3798c4576fd86597a5465109876286be5
btwl <1253102853@qq.com>
2023-10-31 11:12:01 +0800

    wallet apis upload to mainnet

---
+++ .dfx/ic/canisters/backend/backend.did.d.ts
@@ -0,0 +1,136 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
+export interface BallotInfo { 'vote' : number, 'proposal_id' : [] | [NeuronId] }
+export interface CanisterChange {
+  'timestamp_nanos' : bigint,
+  'canister_version' : bigint,
+  'origin' : CanisterChangeOrigin,
+  'details' : CanisterChangeDetails,
+}
+export type CanisterChangeDetails = { 'creation' : CreationRecord } |
+  { 'code_deployment' : CodeDeploymentRecord } |
+  { 'controllers_change' : CreationRecord } |
+  { 'code_uninstall' : null };
+export type CanisterChangeOrigin = { 'from_user' : FromUserRecord } |
+  { 'from_canister' : FromCanisterRecord };
+export interface CanisterInfoResponse {
+  'controllers' : Array<Principal>,
+  'module_hash' : [] | [Uint8Array | number[]],
+  'recent_changes' : Array<CanisterChange>,
+  'total_num_changes' : bigint,
+}
+export type CanisterInstallMode = { 'reinstall' : null } |
+  { 'upgrade' : null } |
+  { 'install' : null };
+export interface CanisterStatusResponse {
+  'status' : CanisterStatusType,
+  'memory_size' : bigint,
+  'cycles' : bigint,
+  'settings' : DefiniteCanisterSettings,
+  'idle_cycles_burned_per_day' : bigint,
+  'module_hash' : [] | [Uint8Array | number[]],
+}
+export type CanisterStatusType = { 'stopped' : null } |
+  { 'stopping' : null } |
+  { 'running' : null };
+export interface CodeDeploymentRecord {
+  'mode' : CanisterInstallMode,
+  'module_hash' : Uint8Array | number[],
+}
+export interface CreationRecord { 'controllers' : Array<Principal> }
+export type CustomResult1 = { 'Ok' : NeuronInfo } |
+  { 'Err' : GovernanceError };
+export interface DefiniteCanisterSettings {
+  'freezing_threshold' : bigint,
+  'controllers' : Array<Principal>,
+  'memory_allocation' : bigint,
+  'compute_allocation' : bigint,
+}
+export interface FromCanisterRecord {
+  'canister_version' : [] | [bigint],
+  'canister_id' : Principal,
+}
+export interface FromUserRecord { 'user_id' : Principal }
+export interface GovernanceError {
+  'error_message' : string,
+  'error_type' : number,
+}
+export interface KnownNeuronData {
+  'name' : string,
+  'description' : [] | [string],
+}
+export interface NeuronId { 'id' : Uint8Array | number[] }
+export interface NeuronInfo {
+  'dissolve_delay_seconds' : bigint,
+  'recent_ballots' : Array<BallotInfo>,
+  'created_timestamp_seconds' : bigint,
+  'state' : number,
+  'stake_e8s' : bigint,
+  'joined_community_fund_timestamp_seconds' : [] | [bigint],
+  'retrieved_at_timestamp_seconds' : bigint,
+  'known_neuron_data' : [] | [KnownNeuronData],
+  'voting_power' : bigint,
+  'age_seconds' : bigint,
+}
+export type RejectionCode = { 'NoError' : null } |
+  { 'CanisterError' : null } |
+  { 'SysTransient' : null } |
+  { 'DestinationInvalid' : null } |
+  { 'Unknown' : null } |
+  { 'SysFatal' : null } |
+  { 'CanisterReject' : null };
+export type Result = { 'Ok' : boolean } |
+  { 'Err' : string };
+export type Result_1 = { 'Ok' : UserProfile } |
+  { 'Err' : string };
+export type Result_2 = { 'Ok' : [CustomResult1] } |
+  { 'Err' : [RejectionCode, string] };
+export type Result_3 = { 'Ok' : WalletProfile } |
+  { 'Err' : string };
+export type Result_4 = { 'Ok' : Array<WalletProfile> } |
+  { 'Err' : Array<WalletProfile> };
+export interface UserProfile {
+  'owner' : Principal,
+  'name' : string,
+  'create_time' : bigint,
+}
+export interface WalletAddCommand {
+  'from' : string,
+  'name' : string,
+  'address' : string,
+}
+export interface WalletProfile {
+  'id' : bigint,
+  'last_transaction_time' : bigint,
+  'last_sync_time' : bigint,
+  'from' : string,
+  'name' : string,
+  'create_time' : bigint,
+  'address' : string,
+  'holder' : Principal,
+  'transactions' : bigint,
+}
+export interface WalletUpdateCommand {
+  'last_transaction_time' : bigint,
+  'last_sync_time' : bigint,
+  'from' : string,
+  'name' : string,
+  'address' : string,
+  'transactions' : bigint,
+}
+export interface _SERVICE {
+  'add_wallet' : ActorMethod<[WalletAddCommand], Result>,
+  'auto_register_user' : ActorMethod<[], Result_1>,
+  'create_and_install' : ActorMethod<[], string>,
+  'delete_wallet' : ActorMethod<[bigint], Result>,
+  'get_canister_info' : ActorMethod<[string], CanisterInfoResponse>,
+  'get_canister_status' : ActorMethod<[string], CanisterStatusResponse>,
+  'get_neuron_info' : ActorMethod<[bigint], Result_2>,
+  'list_all_user' : ActorMethod<[], Array<UserProfile>>,
+  'query_a_wallet' : ActorMethod<[bigint], Result_3>,
+  'query_all_wallets' : ActorMethod<[], Result_4>,
+  'update_wallet' : ActorMethod<[WalletUpdateCommand], Result>,
+  'user_quantity' : ActorMethod<[], number>,
+  'whoami' : ActorMethod<[], Principal>,
+}
---
+++ .dfx/ic/canisters/backend/backend.did.js
@@ -0,0 +1,152 @@
+export const idlFactory = ({ IDL }) => {
+  const WalletAddCommand = IDL.Record({
+    'from' : IDL.Text,
+    'name' : IDL.Text,
+    'address' : IDL.Text,
+  });
+  const Result = IDL.Variant({ 'Ok' : IDL.Bool, 'Err' : IDL.Text });
+  const UserProfile = IDL.Record({
+    'owner' : IDL.Principal,
+    'name' : IDL.Text,
+    'create_time' : IDL.Nat64,
+  });
+  const Result_1 = IDL.Variant({ 'Ok' : UserProfile, 'Err' : IDL.Text });
+  const FromUserRecord = IDL.Record({ 'user_id' : IDL.Principal });
+  const FromCanisterRecord = IDL.Record({
+    'canister_version' : IDL.Opt(IDL.Nat64),
+    'canister_id' : IDL.Principal,
+  });
+  const CanisterChangeOrigin = IDL.Variant({
+    'from_user' : FromUserRecord,
+    'from_canister' : FromCanisterRecord,
+  });
+  const CreationRecord = IDL.Record({ 'controllers' : IDL.Vec(IDL.Principal) });
+  const CanisterInstallMode = IDL.Variant({
+    'reinstall' : IDL.Null,
+    'upgrade' : IDL.Null,
+    'install' : IDL.Null,
+  });
+  const CodeDeploymentRecord = IDL.Record({
+    'mode' : CanisterInstallMode,
+    'module_hash' : IDL.Vec(IDL.Nat8),
+  });
+  const CanisterChangeDetails = IDL.Variant({
+    'creation' : CreationRecord,
+    'code_deployment' : CodeDeploymentRecord,
+    'controllers_change' : CreationRecord,
+    'code_uninstall' : IDL.Null,
+  });
+  const CanisterChange = IDL.Record({
+    'timestamp_nanos' : IDL.Nat64,
+    'canister_version' : IDL.Nat64,
+    'origin' : CanisterChangeOrigin,
+    'details' : CanisterChangeDetails,
+  });
+  const CanisterInfoResponse = IDL.Record({
+    'controllers' : IDL.Vec(IDL.Principal),
+    'module_hash' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'recent_changes' : IDL.Vec(CanisterChange),
+    'total_num_changes' : IDL.Nat64,
+  });
+  const CanisterStatusType = IDL.Variant({
+    'stopped' : IDL.Null,
+    'stopping' : IDL.Null,
+    'running' : IDL.Null,
+  });
+  const DefiniteCanisterSettings = IDL.Record({
+    'freezing_threshold' : IDL.Nat,
+    'controllers' : IDL.Vec(IDL.Principal),
+    'memory_allocation' : IDL.Nat,
+    'compute_allocation' : IDL.Nat,
+  });
+  const CanisterStatusResponse = IDL.Record({
+    'status' : CanisterStatusType,
+    'memory_size' : IDL.Nat,
+    'cycles' : IDL.Nat,
+    'settings' : DefiniteCanisterSettings,
+    'idle_cycles_burned_per_day' : IDL.Nat,
+    'module_hash' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const NeuronId = IDL.Record({ 'id' : IDL.Vec(IDL.Nat8) });
+  const BallotInfo = IDL.Record({
+    'vote' : IDL.Int32,
+    'proposal_id' : IDL.Opt(NeuronId),
+  });
+  const KnownNeuronData = IDL.Record({
+    'name' : IDL.Text,
+    'description' : IDL.Opt(IDL.Text),
+  });
+  const NeuronInfo = IDL.Record({
+    'dissolve_delay_seconds' : IDL.Nat64,
+    'recent_ballots' : IDL.Vec(BallotInfo),
+    'created_timestamp_seconds' : IDL.Nat64,
+    'state' : IDL.Int32,
+    'stake_e8s' : IDL.Nat64,
+    'joined_community_fund_timestamp_seconds' : IDL.Opt(IDL.Nat64),
+    'retrieved_at_timestamp_seconds' : IDL.Nat64,
+    'known_neuron_data' : IDL.Opt(KnownNeuronData),
+    'voting_power' : IDL.Nat64,
+    'age_seconds' : IDL.Nat64,
+  });
+  const GovernanceError = IDL.Record({
+    'error_message' : IDL.Text,
+    'error_type' : IDL.Int32,
+  });
+  const CustomResult1 = IDL.Variant({
+    'Ok' : NeuronInfo,
+    'Err' : GovernanceError,
+  });
+  const RejectionCode = IDL.Variant({
+    'NoError' : IDL.Null,
+    'CanisterError' : IDL.Null,
+    'SysTransient' : IDL.Null,
+    'DestinationInvalid' : IDL.Null,
+    'Unknown' : IDL.Null,
+    'SysFatal' : IDL.Null,
+    'CanisterReject' : IDL.Null,
+  });
+  const Result_2 = IDL.Variant({
+    'Ok' : IDL.Tuple(CustomResult1),
+    'Err' : IDL.Tuple(RejectionCode, IDL.Text),
+  });
+  const WalletProfile = IDL.Record({
+    'id' : IDL.Nat64,
+    'last_transaction_time' : IDL.Nat64,
+    'last_sync_time' : IDL.Nat64,
+    'from' : IDL.Text,
+    'name' : IDL.Text,
+    'create_time' : IDL.Nat64,
+    'address' : IDL.Text,
+    'holder' : IDL.Principal,
+    'transactions' : IDL.Nat64,
+  });
+  const Result_3 = IDL.Variant({ 'Ok' : WalletProfile, 'Err' : IDL.Text });
+  const Result_4 = IDL.Variant({
+    'Ok' : IDL.Vec(WalletProfile),
+    'Err' : IDL.Vec(WalletProfile),
+  });
+  const WalletUpdateCommand = IDL.Record({
+    'last_transaction_time' : IDL.Nat64,
+    'last_sync_time' : IDL.Nat64,
+    'from' : IDL.Text,
+    'name' : IDL.Text,
+    'address' : IDL.Text,
+    'transactions' : IDL.Nat64,
+  });
+  return IDL.Service({
+    'add_wallet' : IDL.Func([WalletAddCommand], [Result], []),
+    'auto_register_user' : IDL.Func([], [Result_1], []),
+    'create_and_install' : IDL.Func([], [IDL.Text], []),
+    'delete_wallet' : IDL.Func([IDL.Nat64], [Result], []),
+    'get_canister_info' : IDL.Func([IDL.Text], [CanisterInfoResponse], []),
+    'get_canister_status' : IDL.Func([IDL.Text], [CanisterStatusResponse], []),
+    'get_neuron_info' : IDL.Func([IDL.Nat64], [Result_2], []),
+    'list_all_user' : IDL.Func([], [IDL.Vec(UserProfile)], []),
+    'query_a_wallet' : IDL.Func([IDL.Nat64], [Result_3], ['query']),
+    'query_all_wallets' : IDL.Func([], [Result_4], ['query']),
+    'update_wallet' : IDL.Func([WalletUpdateCommand], [Result], []),
+    'user_quantity' : IDL.Func([], [IDL.Nat32], []),
+    'whoami' : IDL.Func([], [IDL.Principal], ['query']),
+  });
+};
+export const init = ({ IDL }) => { return []; };
