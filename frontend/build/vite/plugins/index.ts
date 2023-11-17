6827b0519906da6e1cd5564da4e7be320e94139d
e274426380 <867690443@qq.com>
2023-11-17 14:57:02 +0800

    remove @vitejs/plugin-legacy for avoid bigint error

--- frontend/build/vite/plugins/index.ts
+++ frontend/build/vite/plugins/index.ts
@@ -1,37 +1,38 @@
-import type { Plugin } from 'vite';
+import type { Plugin } from "vite"
 
-import type { ViteEnv } from '../../../types/model';
+import type { ViteEnv } from "../../../types/model"
 
-import vue from '@vitejs/plugin-vue';
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-import { viteHtmlPlugins } from './html';
-import { viteCompressionPlugin } from './compression';
-import { legacyPlugin } from "./legacy";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin"
+import vue from "@vitejs/plugin-vue"
+import { viteCompressionPlugin } from "./compression"
+import { viteHtmlPlugins } from "./html"
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
-    const {
-        VITE_BUILD_COMPRESS: compressType,
-        VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: shouldBuildCompressDeleteFile,
-    } = viteEnv;
+  const {
+    VITE_BUILD_COMPRESS: compressType,
+    VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: shouldBuildCompressDeleteFile,
+  } = viteEnv
 
-    const vitePlugins: (Plugin | Plugin[])[] = [];
+  const vitePlugins: (Plugin | Plugin[])[] = []
 
-    vitePlugins.push(vue({
-        template: {transformAssetUrls}
-    }));
+  vitePlugins.push(
+    vue({
+      template: { transformAssetUrls },
+    }),
+  )
+  vitePlugins.push(
+    quasar({
+      sassVariables: "frontend/assets/css/quasar-variables.sass",
+    }),
+  )
+  vitePlugins.push(...viteHtmlPlugins(viteEnv, isBuild)) // 注入配置字符串
+
+  if (isBuild) {
+    // 生成模式执行
     vitePlugins.push(
-        quasar({
-            sassVariables: 'frontend/assets/css/quasar-variables.sass'
-        }));
-    vitePlugins.push(...viteHtmlPlugins(viteEnv, isBuild)); // 注入配置字符串
-    vitePlugins.push(legacyPlugin()); // 低级浏览器支持
+      viteCompressionPlugin(compressType, shouldBuildCompressDeleteFile), // 压缩
+    )
+  }
 
-    if (isBuild) {
-        // 生成模式执行
-        vitePlugins.push(
-            viteCompressionPlugin(compressType, shouldBuildCompressDeleteFile), // 压缩
-        );
-    }
-
-    return vitePlugins;
+  return vitePlugins
 }
