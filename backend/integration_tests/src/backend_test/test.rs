222932a76c33d799b28322686d40404ec43d7fe9
btwl <1253102853@qq.com>
2023-11-28 19:06:04 +0800

    empty

--- backend/integration_tests/src/backend_test/test.rs
+++ backend/integration_tests/src/backend_test/test.rs
@@ -1,11 +1,8 @@
 use crate::client::setup::setup_new_env;
 
 #[test]
-fn create_a_nns_wallet() {
+pub fn create_a_nns_wallet() {
     // TODO set once for all or set new everytime ?
     setup_new_env();
-
-    
-
 
 }
