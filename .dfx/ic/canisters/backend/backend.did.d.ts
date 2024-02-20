5a73fb9948f9295f803602c07c194b89dae31a64
btwl <1253102853@qq.com>
2024-02-20 11:40:36 +0800

    did upload

---
+++ .dfx/ic/canisters/backend/backend.did.d.ts
@@ -0,0 +1,162 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
+export interface AddRecordCommand {
+  'tag' : string,
+  'time' : bigint,
+  't_type' : string,
+  'comment' : string,
+  'address' : string,
+  'manual' : boolean,
+  'principal_id' : [] | [string],
+  'price' : number,
+  'amount' : number,
+}
+export interface BallotInfo { 'vote' : number, 'proposal_id' : [] | [NeuronId] }
+export type CustomResult1 = { 'Ok' : NeuronInfo } |
+  { 'Err' : GovernanceError };
+export interface EditHistoryCommand {
+  'id' : bigint,
+  'tag' : string,
+  'time' : bigint,
+  't_type' : string,
+  'comment' : string,
+  'manual' : boolean,
+  'price' : number,
+  'amount' : number,
+}
+export interface GovernanceError {
+  'error_message' : string,
+  'error_type' : number,
+}
+export interface HistoryQueryCommand {
+  'tag' : string,
+  'from_time' : bigint,
+  'to_time' : bigint,
+  't_type' : string,
+  'sort_method' : string,
+  'address' : [] | [string],
+}
+export interface KnownNeuronData {
+  'name' : string,
+  'description' : [] | [string],
+}
+export interface NeuronAddCommand {
+  'from' : string,
+  'name' : string,
+  'address' : string,
+}
+export interface NeuronId { 'id' : Uint8Array | number[] }
+export interface NeuronInfo {
+  'dissolve_delay_seconds' : bigint,
+  'recent_ballots' : Array<BallotInfo>,
+  'created_timestamp_seconds' : bigint,
+  'state' : number,
+  'stake_e8s' : bigint,
+  'joined_community_fund_timestamp_seconds' : [] | [bigint],
+  'retrieved_at_timestamp_seconds' : bigint,
+  'known_neuron_data' : [] | [KnownNeuronData],
+  'voting_power' : bigint,
+  'age_seconds' : bigint,
+}
+export interface NeuronProfile {
+  'id' : bigint,
+  'owner' : Principal,
+  'name' : string,
+  'update_time' : bigint,
+  'create_time' : bigint,
+  'address' : string,
+}
+export interface NeuronUpdateCommand { 'id' : bigint, 'name' : string }
+export interface RecordProfile {
+  'id' : bigint,
+  'tag' : string,
+  'time' : bigint,
+  't_type' : string,
+  'comment' : string,
+  'address' : string,
+  'manual' : boolean,
+  'principal_id' : [] | [string],
+  'price' : number,
+  'amount' : number,
+}
+export type RejectionCode = { 'NoError' : null } |
+  { 'CanisterError' : null } |
+  { 'SysTransient' : null } |
+  { 'DestinationInvalid' : null } |
+  { 'Unknown' : null } |
+  { 'SysFatal' : null } |
+  { 'CanisterReject' : null };
+export type Result = { 'Ok' : boolean } |
+  { 'Err' : string };
+export type Result_1 = { 'Ok' : bigint } |
+  { 'Err' : string };
+export type Result_2 = { 'Ok' : UserProfile } |
+  { 'Err' : string };
+export type Result_3 = { 'Ok' : [CustomResult1] } |
+  { 'Err' : [RejectionCode, string] };
+export type Result_4 = { 'Ok' : NeuronProfile } |
+  { 'Err' : string };
+export type Result_5 = { 'Ok' : WalletProfile } |
+  { 'Err' : string };
+export type Result_6 = { 'Ok' : Array<NeuronProfile> } |
+  { 'Err' : Array<NeuronProfile> };
+export type Result_7 = { 'Ok' : Array<WalletProfile> } |
+  { 'Err' : Array<WalletProfile> };
+export type Result_8 = { 'Ok' : Array<[string, Array<RecordProfile>]> } |
+  { 'Err' : string };
+export interface UserProfile {
+  'owner' : Principal,
+  'name' : string,
+  'create_time' : bigint,
+}
+export interface WalletAddCommand {
+  'from' : string,
+  'name' : string,
+  'address' : string,
+  'principal_id' : [] | [string],
+}
+export interface WalletProfile {
+  'id' : bigint,
+  'last_transaction_time' : bigint,
+  'last_sync_time' : bigint,
+  'from' : string,
+  'name' : string,
+  'create_time' : bigint,
+  'address' : string,
+  'principal_id' : [] | [string],
+  'holder' : Principal,
+  'transactions' : bigint,
+}
+export interface WalletUpdateCommand {
+  'id' : bigint,
+  'from' : string,
+  'name' : string,
+}
+export interface _SERVICE {
+  'add_neuron_wallet' : ActorMethod<[NeuronAddCommand], Result>,
+  'add_transaction_record' : ActorMethod<[AddRecordCommand], Result_1>,
+  'add_wallet' : ActorMethod<[WalletAddCommand], Result>,
+  'auto_register_user' : ActorMethod<[], Result_2>,
+  'delete_neuron_wallet' : ActorMethod<[bigint], Result>,
+  'delete_transaction_record' : ActorMethod<[bigint], Result_1>,
+  'delete_wallet' : ActorMethod<[bigint], Result>,
+  'do_post_upgrade' : ActorMethod<[string], boolean>,
+  'do_pre_upgrade_and_print_db' : ActorMethod<[], string>,
+  'edit_transaction_record' : ActorMethod<[EditHistoryCommand], Result>,
+  'get_balance' : ActorMethod<[], bigint>,
+  'get_neuron_info' : ActorMethod<[bigint], Result_3>,
+  'list_all_user' : ActorMethod<[], Array<UserProfile>>,
+  'query_a_neuron_wallet' : ActorMethod<[bigint], Result_4>,
+  'query_a_wallet' : ActorMethod<[bigint], Result_5>,
+  'query_all_neuron_wallet' : ActorMethod<[], Result_6>,
+  'query_all_wallets' : ActorMethod<[], Result_7>,
+  'sync_transaction_record' : ActorMethod<
+    [Array<[bigint, Array<RecordProfile>]>],
+    Result
+  >,
+  'update_neuron_wallet' : ActorMethod<[NeuronUpdateCommand], Result>,
+  'update_wallet' : ActorMethod<[WalletUpdateCommand], Result>,
+  'user_quantity' : ActorMethod<[], number>,
+  'wallet_history' : ActorMethod<[HistoryQueryCommand], Result_8>,
+}
---
+++ .dfx/ic/canisters/backend/backend.did.js
@@ -0,0 +1,176 @@
+export const idlFactory = ({ IDL }) => {
+  const NeuronAddCommand = IDL.Record({
+    'from' : IDL.Text,
+    'name' : IDL.Text,
+    'address' : IDL.Text,
+  });
+  const Result = IDL.Variant({ 'Ok' : IDL.Bool, 'Err' : IDL.Text });
+  const AddRecordCommand = IDL.Record({
+    'tag' : IDL.Text,
+    'time' : IDL.Nat64,
+    't_type' : IDL.Text,
+    'comment' : IDL.Text,
+    'address' : IDL.Text,
+    'manual' : IDL.Bool,
+    'principal_id' : IDL.Opt(IDL.Text),
+    'price' : IDL.Float64,
+    'amount' : IDL.Nat32,
+  });
+  const Result_1 = IDL.Variant({ 'Ok' : IDL.Nat64, 'Err' : IDL.Text });
+  const WalletAddCommand = IDL.Record({
+    'from' : IDL.Text,
+    'name' : IDL.Text,
+    'address' : IDL.Text,
+    'principal_id' : IDL.Opt(IDL.Text),
+  });
+  const UserProfile = IDL.Record({
+    'owner' : IDL.Principal,
+    'name' : IDL.Text,
+    'create_time' : IDL.Nat64,
+  });
+  const Result_2 = IDL.Variant({ 'Ok' : UserProfile, 'Err' : IDL.Text });
+  const EditHistoryCommand = IDL.Record({
+    'id' : IDL.Nat64,
+    'tag' : IDL.Text,
+    'time' : IDL.Nat64,
+    't_type' : IDL.Text,
+    'comment' : IDL.Text,
+    'manual' : IDL.Bool,
+    'price' : IDL.Float64,
+    'amount' : IDL.Nat32,
+  });
+  const NeuronId = IDL.Record({ 'id' : IDL.Vec(IDL.Nat8) });
+  const BallotInfo = IDL.Record({
+    'vote' : IDL.Int32,
+    'proposal_id' : IDL.Opt(NeuronId),
+  });
+  const KnownNeuronData = IDL.Record({
+    'name' : IDL.Text,
+    'description' : IDL.Opt(IDL.Text),
+  });
+  const NeuronInfo = IDL.Record({
+    'dissolve_delay_seconds' : IDL.Nat64,
+    'recent_ballots' : IDL.Vec(BallotInfo),
+    'created_timestamp_seconds' : IDL.Nat64,
+    'state' : IDL.Int32,
+    'stake_e8s' : IDL.Nat64,
+    'joined_community_fund_timestamp_seconds' : IDL.Opt(IDL.Nat64),
+    'retrieved_at_timestamp_seconds' : IDL.Nat64,
+    'known_neuron_data' : IDL.Opt(KnownNeuronData),
+    'voting_power' : IDL.Nat64,
+    'age_seconds' : IDL.Nat64,
+  });
+  const GovernanceError = IDL.Record({
+    'error_message' : IDL.Text,
+    'error_type' : IDL.Int32,
+  });
+  const CustomResult1 = IDL.Variant({
+    'Ok' : NeuronInfo,
+    'Err' : GovernanceError,
+  });
+  const RejectionCode = IDL.Variant({
+    'NoError' : IDL.Null,
+    'CanisterError' : IDL.Null,
+    'SysTransient' : IDL.Null,
+    'DestinationInvalid' : IDL.Null,
+    'Unknown' : IDL.Null,
+    'SysFatal' : IDL.Null,
+    'CanisterReject' : IDL.Null,
+  });
+  const Result_3 = IDL.Variant({
+    'Ok' : IDL.Tuple(CustomResult1),
+    'Err' : IDL.Tuple(RejectionCode, IDL.Text),
+  });
+  const NeuronProfile = IDL.Record({
+    'id' : IDL.Nat64,
+    'owner' : IDL.Principal,
+    'name' : IDL.Text,
+    'update_time' : IDL.Nat64,
+    'create_time' : IDL.Nat64,
+    'address' : IDL.Text,
+  });
+  const Result_4 = IDL.Variant({ 'Ok' : NeuronProfile, 'Err' : IDL.Text });
+  const WalletProfile = IDL.Record({
+    'id' : IDL.Nat64,
+    'last_transaction_time' : IDL.Nat64,
+    'last_sync_time' : IDL.Nat64,
+    'from' : IDL.Text,
+    'name' : IDL.Text,
+    'create_time' : IDL.Nat64,
+    'address' : IDL.Text,
+    'principal_id' : IDL.Opt(IDL.Text),
+    'holder' : IDL.Principal,
+    'transactions' : IDL.Nat64,
+  });
+  const Result_5 = IDL.Variant({ 'Ok' : WalletProfile, 'Err' : IDL.Text });
+  const Result_6 = IDL.Variant({
+    'Ok' : IDL.Vec(NeuronProfile),
+    'Err' : IDL.Vec(NeuronProfile),
+  });
+  const Result_7 = IDL.Variant({
+    'Ok' : IDL.Vec(WalletProfile),
+    'Err' : IDL.Vec(WalletProfile),
+  });
+  const RecordProfile = IDL.Record({
+    'id' : IDL.Nat64,
+    'tag' : IDL.Text,
+    'time' : IDL.Nat64,
+    't_type' : IDL.Text,
+    'comment' : IDL.Text,
+    'address' : IDL.Text,
+    'manual' : IDL.Bool,
+    'principal_id' : IDL.Opt(IDL.Text),
+    'price' : IDL.Float64,
+    'amount' : IDL.Nat32,
+  });
+  const NeuronUpdateCommand = IDL.Record({
+    'id' : IDL.Nat64,
+    'name' : IDL.Text,
+  });
+  const WalletUpdateCommand = IDL.Record({
+    'id' : IDL.Nat64,
+    'from' : IDL.Text,
+    'name' : IDL.Text,
+  });
+  const HistoryQueryCommand = IDL.Record({
+    'tag' : IDL.Text,
+    'from_time' : IDL.Nat64,
+    'to_time' : IDL.Nat64,
+    't_type' : IDL.Text,
+    'sort_method' : IDL.Text,
+    'address' : IDL.Opt(IDL.Text),
+  });
+  const Result_8 = IDL.Variant({
+    'Ok' : IDL.Vec(IDL.Tuple(IDL.Text, IDL.Vec(RecordProfile))),
+    'Err' : IDL.Text,
+  });
+  return IDL.Service({
+    'add_neuron_wallet' : IDL.Func([NeuronAddCommand], [Result], []),
+    'add_transaction_record' : IDL.Func([AddRecordCommand], [Result_1], []),
+    'add_wallet' : IDL.Func([WalletAddCommand], [Result], []),
+    'auto_register_user' : IDL.Func([], [Result_2], []),
+    'delete_neuron_wallet' : IDL.Func([IDL.Nat64], [Result], []),
+    'delete_transaction_record' : IDL.Func([IDL.Nat64], [Result_1], []),
+    'delete_wallet' : IDL.Func([IDL.Nat64], [Result], []),
+    'do_post_upgrade' : IDL.Func([IDL.Text], [IDL.Bool], []),
+    'do_pre_upgrade_and_print_db' : IDL.Func([], [IDL.Text], ['query']),
+    'edit_transaction_record' : IDL.Func([EditHistoryCommand], [Result], []),
+    'get_balance' : IDL.Func([], [IDL.Nat64], []),
+    'get_neuron_info' : IDL.Func([IDL.Nat64], [Result_3], []),
+    'list_all_user' : IDL.Func([], [IDL.Vec(UserProfile)], []),
+    'query_a_neuron_wallet' : IDL.Func([IDL.Nat64], [Result_4], ['query']),
+    'query_a_wallet' : IDL.Func([IDL.Nat64], [Result_5], ['query']),
+    'query_all_neuron_wallet' : IDL.Func([], [Result_6], ['query']),
+    'query_all_wallets' : IDL.Func([], [Result_7], ['query']),
+    'sync_transaction_record' : IDL.Func(
+        [IDL.Vec(IDL.Tuple(IDL.Nat64, IDL.Vec(RecordProfile)))],
+        [Result],
+        [],
+      ),
+    'update_neuron_wallet' : IDL.Func([NeuronUpdateCommand], [Result], []),
+    'update_wallet' : IDL.Func([WalletUpdateCommand], [Result], []),
+    'user_quantity' : IDL.Func([], [IDL.Nat32], []),
+    'wallet_history' : IDL.Func([HistoryQueryCommand], [Result_8], ['query']),
+  });
+};
+export const init = ({ IDL }) => { return []; };
