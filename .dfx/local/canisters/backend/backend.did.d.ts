7068e11860c5c14a3fec52c36d44be8e8dc968e9
btwl <1253102853@qq.com>
2024-03-04 11:41:22 +0800

    upgrade production bakcend  env code

--- .dfx/local/canisters/backend/backend.did.d.ts
+++ .dfx/local/canisters/backend/backend.did.d.ts
@@ -275,4 +275,4 @@ export interface _SERVICE {
   'wallet_history' : ActorMethod<[HistoryQueryCommand], Result_9>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
-export declare const init: ({ IDL }: { IDL: IDL }) => IDL.Type[];
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
