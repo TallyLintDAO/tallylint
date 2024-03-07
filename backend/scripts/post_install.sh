d73d0dcd53ef5639bc25bbebe5c5ae968cea7833
btwl <1253102853@qq.com>
2024-03-07 09:24:54 +0800

    improve CI

--- backend/scripts/post_install.sh
+++ backend/scripts/post_install.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 echo post_install task: candid-extractor
 candid-extractor target/wasm32-unknown-unknown/release/backend.wasm >./backend/canisters/backend/backend.did
+
+
+base_dir=$(git rev-parse --show-toplevel)
+echo "The project root directory is ${base_dir}"
+
+cd $base_dir
+cd ./.dfx/ic/canisters/backend
+mv service.did backend.did
+mv service.did.d.ts backend.did.d.ts
+mv service.did.js backend.did.js
+
+# mv会覆盖写入已存在的同名文件
+cd $base_dir
+cd ./.dfx/local/canisters/backend
+mv service.did backend.did
+mv service.did.d.ts backend.did.d.ts
+mv service.did.js backend.did.js
+
+cd $base_dir
+git add -f ./.dfx/ic/canisters/backend/backend.did 
+git add -f ./.dfx/ic/canisters/backend/backend.did.d.ts  
+git add -f ./.dfx/ic/canisters/backend/backend.did.js 
+git add -f ./.dfx/ic/canisters/backend/index.js 
+
+cd $base_dir
+git add -f ./.dfx/local/canisters/backend/backend.did
+git add -f ./.dfx/local/canisters/backend/backend.did.d.ts  
+git add -f ./.dfx/local/canisters/backend/backend.did.js
+git add -f ./.dfx/local/canisters/backend/index.js
+
+# TODO might need CI git auto commit and push .
--- reproducible/continue_prep_env.sh
+++ reproducible/continue_prep_env.sh
@@ -2,6 +2,7 @@
 
 # this downlaod master branch.
 git clone https://github.com/TaxLintDAO/taxlint.git --depth=1 && cd taxlint 
+cargo install candid-extractor
 cargo check && cargo build --target wasm32-unknown-unknown --release -p backend --locked
 echo -e "\033[32m #### if you see wasm file below. then dev_env is ready! #### \n \033[0m"
 ls ./target/wasm32-unknown-unknown/release/backend.wasm
@@ -12,3 +13,4 @@ ls ./target/wasm32-unknown-unknown/release/backend.wasm
 # dfx --version
 # dfx start --background
 # dfx deploy backend
+
