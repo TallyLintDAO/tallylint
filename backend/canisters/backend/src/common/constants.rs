ed25924b6e8f83dd493f8e5ff202b104ff0d8393
ydr <18081231054@163.com>
2024-07-03 01:13:00 +0800

    uninstall data recovery and restore

--- backend/canisters/backend/src/common/constants.rs
+++ backend/canisters/backend/src/common/constants.rs
@@ -1,8 +1,6 @@
 pub const PROXY_CANISTER_ID: &str = "iustv-tiaaa-aaaao-a3aga-cai";
 pub const GOVERNANCE_ZHOU: &str =
   "gq66f-io24f-torxu-ftmfi-chzvv-umdni-3jkai-s734u-wdzps-jmlkk-2ae";
-pub const GOVERNANCE_BTWL: &str =
-  "b76rz-axcfs-swjig-bzzpx-yt5g7-2vcpg-wmb7i-2mz7s-upd4f-mag4c-yae";
 pub const BACKEND_CANISTER_ID: &str = "bkyz2-fmaaa-aaaaa-qaaaq-cai";
 pub const GOVERNANCE_YINDR: &str =
   "piqt3-wn7ax-jf26p-rmaq3-c3svs-a6j6r-y3p2b-37rsf-jntb5-c5uy4-7ae";
--- backend/canisters/backend/src/common/mod.rs
+++ backend/canisters/backend/src/common/mod.rs
@@ -1,5 +1,4 @@
 pub mod constants;
 pub mod context;
 pub mod guard;
-pub mod life_cycle;
 pub mod times;
