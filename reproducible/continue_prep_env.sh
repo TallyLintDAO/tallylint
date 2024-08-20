3a7365e0462f4ce8ae73589a990a261cf12c1c21
e274426380 <867690443@qq.com>
2024-08-20 15:31:42 +0800

    add domains www.tallylint.online

--- reproducible/continue_prep_env.sh
+++ reproducible/continue_prep_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this downlaod master branch.
-git clone https://github.com/TaxLintDAO/taxlint.git --depth=1 && cd taxlint 
+git clone https://github.com/TallyLintDAO/tallylint.git --depth=1 && cd tallylint
 cargo install candid-extractor
 cargo check && cargo build --target wasm32-unknown-unknown --release -p backend --locked
 echo -e "\033[32m #### if you see wasm file below. then dev_env is ready! #### \n \033[0m"
