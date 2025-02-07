6ee34c1461010cab1aba2664acec592b2a7ada35
e274426380 <867690443@qq.com>
2025-02-07 19:12:10 +0800

    improve predict post form

--- frontend/views/market/post-predict/PostPredict.vue
+++ frontend/views/market/post-predict/PostPredict.vue
@@ -1,7 +1,69 @@
 <template>
-  <div class="market-container">Predict</div>
+  <div class="predict-container q-pt-lg">
+    <div class="container">
+      <q-form @submit="onSubmit" ref="form" class="q-gutter-md">
+        <q-select
+          filled
+          v-model="predict.model"
+          :options="models"
+          label="Predict Model Version"
+        />
+        <q-select
+          filled
+          v-model="predict.model"
+          :options="models"
+          label="Predict Model Parameters"
+        />
+        <q-input
+          filled
+          v-model="predict.model"
+          label="Wallet Address *"
+          hint="Input Principal ID"
+          lazy-rules
+        />
+        <q-input
+          filled
+          disable
+          label="Wallet Account ID"
+          v-model="predict.model"
+        />
+        <q-input
+          filled
+          v-model="predict.model"
+          label="Wallet Name *"
+          hint="Identify your wallet quickly"
+          lazy-rules
+          :rules="[
+                (val: string) => (val && val.length > 0) || 'Please input wallet name',
+              ]"
+        />
+        <div class="q-gutter-sm justify-end flex">
+          <q-btn flat label="Cancel" v-close-popup="true" />
+          <q-btn
+            :loading="loading"
+            label="Submit"
+            type="submit"
+            color="primary"
+          />
+        </div>
+      </q-form>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { QForm } from "quasar"
+import { ref } from "vue"
+const form = ref<QForm | null>(null)
+const loading = ref(false)
+const predict = ref({ model: "LSTM" })
+const models = ["LSTM"]
+
+const onSubmit = async () => {
+  loading.value = true
+
+  loading.value = false
+}
+</script>
 
 <style lang="scss" scoped></style>
